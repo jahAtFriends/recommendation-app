bdb9fde0fc6535216d44b085a0924edfb6fdbf64
jahAtFriends <jhammer@friendsbalt.org>
2023-11-15 20:26:48 -0500

    Add absolute path for file input/output in CSV processor

--- src/utils/csv_processor.py
+++ src/utils/csv_processor.py
@@ -1,11 +1,13 @@
 import csv
+import os
 
 def read_csv(file_path):
     """
     Reads a CSV file and returns a list of dictionaries.
     Each dictionary represents a row in the CSV file.
     """
-    with open(file_path, 'r') as file:
+    abs_path = os.path.join(os.getcwd(), 'data', file_path)
+    with open(abs_path, 'r') as file:
         reader = csv.DictReader(file)
         return list(reader)
 
@@ -14,7 +16,8 @@ def write_csv(file_path, data, fieldnames):
     Writes a list of dictionaries to a CSV file.
     Each dictionary in the list represents a row in the CSV file.
     """
-    with open(file_path, 'w', newline='') as file:
+    abs_path = os.path.join(os.getcwd(), 'data', file_path)
+    with open(abs_path, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
