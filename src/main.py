713101b52120c10e64ce68fcc74cea7aeddddbeb
jahAtFriends <jhammer@friendsbalt.org>
2023-11-21 20:20:34 -0500

    Remove unnecessary code

--- src/main.py
+++ src/main.py
@@ -4,13 +4,9 @@ from student import Student
 from utils.csv_processor import read_csv, write_csv
 from utils.yaml_processor import read_yaml
 
-courses = []
-students = {}
-
 #Change this to the current year
 # In any given school year, the year is the year that the school year ends.
 current_year = 2023
-
 
 def main():
     flow_data = read_yaml('courseflow.yaml')
