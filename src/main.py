335b5d85bbb06c6d4a1f37d62f7881cfb8240049
jahAtFriends <jhammer@friendsbalt.org>
2024-02-06 20:40:04 -0500

    Fix roster building and add readout info

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ from recommendations import get_all_recommendations
 from courses import Course_List
 from utils.csv_processor import read_csv, write_csv
 from utils.yaml_processor import read_yaml
+import time
 
 #Change this to the current year
 # In any given school year, the year is the year that the school year ends.
 current_year = 2024
 
 def main():
+    start_time = time.time()
     structural_data = read_yaml('courseflow.yaml')
     course_data = structural_data['courses']
     flow_data = structural_data['flow']
@@ -19,24 +21,27 @@ def main():
     course_list = Course_List.build_course_list(course_data)
 
     #Build the Course Flow
-    course_flow = CourseFlow()
-    course_flow.build_flow(flow_data, course_list)
+    course_flow = CourseFlow(flow_data, course_list)
 
     #Apply prerequisites to courses
     course_list.apply_prerequisites_to_courses(prereq_data)
 
     academic_data = read_csv('academic_data.csv')
-    roster = Roster()
-    roster.build_roster_from_data(academic_data)
+    roster = Roster(current_year)
+    roster.build_roster_from_data(academic_data, course_list)
     
     recommendations = {}
     recommendations = get_all_recommendations(roster, course_flow, current_year)
     
     #convert recommendations to friendly dictionaries
-    csv_recs = [{'student': student, 'recommendations': recommendations[student]} for student in recommendations.keys()]
+    csv_recs = [{'student': student.name, 'recommendations': [course.name for course in recommendations[student]]} for student in recommendations.keys()]
 
     # Write recommendations to CSV
     write_csv('recommendations.csv', csv_recs, ['student', 'recommendations'])
+
+    end_time = time.time()
+    elapsed_time = round(end_time - start_time, 3)
+    print(f'Recommendations completed in approximately {elapsed_time} seconds')
 
 if __name__ == '__main__':
     main()
